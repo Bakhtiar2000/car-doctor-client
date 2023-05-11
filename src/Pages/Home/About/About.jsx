a61ef6d45ca1846bfb5bbea3c06e56a8eb0ed78c
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-05-11 11:26:16 +0600

    added login page and services page

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -9,10 +9,12 @@ const About = () => {
                 <img src={person} className="w-3/4 rounded-lg shadow-2xl" />
                 <img src={parts} className="absolute right-20 top-1/2 border-[8px] w-1/2 border-white object-cover rounded-lg shadow-2xl" />
                 </div>
-                <div className='lg:w-1/2'>
-                    <h1 className="text-4xl text-orange-500 font-bold">About Us</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                <div className='lg:w-1/2 space-y-5 p-4'>
+                    <h1 className="text-4xl text-orange-500 font-semibold">About Us</h1>
+                    <h1 className='text-6xl font-bold w-4/5'>We are qualified & of experience in this field</h1>
+                    <p className="py-2">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+                    <p className="py-2">the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+                    <button className="btn btn-warning">Get More Info</button>
                 </div>
             </div>
         </div>
