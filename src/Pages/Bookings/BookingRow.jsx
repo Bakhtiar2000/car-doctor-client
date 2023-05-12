b4885e33ba56bffcddd720758bf1c6d10bb53f76
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-05-12 15:07:27 +0600

    added confirm functionality

--- src/Pages/Bookings/BookingRow.jsx
+++ src/Pages/Bookings/BookingRow.jsx
@@ -1,19 +1,11 @@
 
-const BookingRow = ({ booking }) => {
-    const { _id, service, date, price, img } = booking
-    const handleDelete= id=>{
-        const proceed= confirm('Are you sure to delete?')
-        if(proceed){
-            fetch(``)
-            .then(res=> res.json())
-            .then(data=> console.log(data))
-        }
-    }
+const BookingRow = ({ booking, handleDelete, handleBookingConfirm }) => {
+    const { _id, service, date, price, img, status } = booking
 
     return (
         <tr>
             <th>
-                <button onClick={()=> handleDelete(_id)} className="btn btn-sm btn-circle">
+                <button onClick={() => handleDelete(_id)} className="btn btn-sm btn-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                 </button>
             </th>
@@ -30,7 +22,11 @@ const BookingRow = ({ booking }) => {
             <td>{date}</td>
             <td>${price}</td>
             <th>
-                <button className="btn btn-ghost btn-xs">details</button>
+               {
+                status === 'confirm' ?
+                <span className="font-bold text-primary">Confirmed</span> :
+                <button onClick={() => handleBookingConfirm(_id)} className="btn btn-ghost btn-xs">Confirm</button>
+               }
             </th>
         </tr>
     );
--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -5,7 +5,7 @@ import BookingRow from "./BookingRow";
 
 const Bookings = () => {
     const { user } = useContext(AuthContext)
-    const [Bookings, setBookings] = useState([])
+    const [bookings, setBookings] = useState([])
     const url = `http://localhost:5000/bookings?email=${user?.email}`
 
     useEffect(() => {
@@ -14,9 +14,49 @@ const Bookings = () => {
             .then(data => setBookings(data))
     }, [])
 
+    const handleDelete= id=>{
+        const proceed= confirm('Are you sure to delete?')
+        if(proceed){
+            fetch(`http://localhost:5000/bookings/${id}`,{
+                method: "DELETE"
+            })
+            .then(res=> res.json())
+            .then(data=> {
+                console.log(data)
+                if(data.deletedCount> 0){
+                    const remaining= bookings.filter(booking=> booking._id !== id)
+                    setBookings(remaining)
+                }
+            })
+        }
+    }
+
+    const handleBookingConfirm= id=>{
+        console.log(id)
+        fetch(`http://localhost:5000/bookings/${id}`,{
+                method: "PATCH",
+                headers:{
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify({status: "confirm"})
+            })
+            .then(res=> res.json())
+            .then(data=> {
+                console.log(data)
+                if(data.modifiedCount> 0){
+                    const remaining= bookings.filter(booking=> booking._id !== id);
+                    const updated= bookings.find(booking=> booking._id === id);
+                    updated.status= 'confirm'
+                    const newBookings= [updated, ...remaining]
+                    setBookings(newBookings)
+                    alert('updated successfully')
+                }
+            })
+    }
+
     return (
         <div>
-            <h2 className="text-5xl">Your bookings: {Bookings.length}</h2>
+            <h2 className="text-5xl">Your bookings: {bookings.length}</h2>
 
             <div className="overflow-x-auto w-full">
                 <table className="table w-full">
@@ -37,9 +77,11 @@ const Bookings = () => {
                     </thead>
                     <tbody>
                         {
-                            Bookings.map(booking=> <BookingRow
+                            bookings.map(booking=> <BookingRow
                                 key={booking._id}
                                 booking={booking}
+                                handleDelete={handleDelete}
+                                handleBookingConfirm={handleBookingConfirm}
                             ></BookingRow>)
                         }
                     </tbody>
